b1f5d746cd8e50caadc4b5782d0ac65e42eb6ac2
dhavakumaran <ub.dhavakumaran@gmail.com>
2020-05-18 22:43:31 +0530

    initial commit

---
+++ dhava.py
@@ -0,0 +1,13 @@
+print(2+3)
+print(2*3)
+print(2**3)
+print(2/3)
+print(2%3)
+print(3/2)
+a=5
+b=7
+if a==b:
+    print("a is equal to b")
+    print("still under statement")
+else:
+ print("a is not equal to b")
